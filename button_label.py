5b00f724b5ca44cf5f4022cf78502326f216e14a
Jigisha Pawar <jigisha.p16@gmail.com>
2022-12-27 10:37:10 +0000

    Button & Label

---
+++ button_label.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from random import randint
+app=Tk()
+app.title('Random Generator')
+app.geometry('350x100')
+
+def show_msg():
+    msg=Label(app,text="Have a nice Day")
+    msg.pack()
+    
+def show_msg1():
+    msg=Label(app,text=randint(1,100))
+    msg.pack()
+    
+b= Button(app,text='Click Me',command=show_msg)
+b.pack()
+
+b1= Button(app,text='Click',command=show_msg1)
+b1.pack()
+
+app.mainloop()
